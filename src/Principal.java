ff36e7f2cc78f9d5a25217e6c4246d1915448c8c
sam-pace <sl.spacce2000@gmail.com>
2023-01-31 14:28:40 -0300

    Criação da classe ConverteMoedas

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String opcoes = JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null,
+				new Object[] { "Conversor de Moeda", "Conversor de temperatura" }, "Escolha").toString();
 
 	}
 
